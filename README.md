565c90af65e090d318ac819c56b54acad94c8be5
vvvvvx <viaco.xu@gmail.com>
2025-10-13 14:30:10 +0800

    update Readme

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
+# Croc-GUI
 
-Project Begin Time：2025-10-04
+> Project Begin Time：2025-10-04
 
+
+
+## Overview
+
+Croc-gui base on schollz/croc. Easily and securely send things from one computer to another. No file size limit. For windows, Linux, MacOS. Developed using Tauri+Rust+Vue.
+
+1. Multi transfer tasks
+
+![MultiTasks](./MultiTasks.png)
+
+2. Send folder
+
+![SendFolder](./SendFolder.png)
+
+3. Send file
+
+![SendFile](./SendFile.png)
+
+4. Receive file
+
+![FileReceive](./FileReceive.png)
+
+5. Text chat
+
+![TextChat](./TextChat.png)
+
+
+
+
+
+## Compile
+
+After install rust and npm,run the commands below：  
+npm install  
+npm run tauri build
+
+## Install
+
+Install croc first or download the zip file here then unzip it. The zip file contains croc and croc-gui.
--- src/App.vue
+++ src/App.vue
@@ -189,7 +189,9 @@ async function selectFile() {
     //sendPaths.value = [];
     tempPaths.value = [];
   }
-  newProcess();
+  if(tempPaths.value.length!==0){
+    newProcess();
+  }
   //console.log(sendPaths);
   console.log("selectFile(),tempPaths:",tempPaths.value);
 }
@@ -798,7 +800,7 @@ When sending,enter custom Code or leave it blank to generate Code automatically.
 When receiving,enter the Code provided by other side.&#10;When transmitting continuously or back and forth,the Code can be kept unchanged." 
           placeholder="">
           <input type="text" v-model="memo"  class="form-control flex-shrink-0" style = "width:80px;flex:0 0 100px;text-align:center;" title="Code别名，用于多任务切换时便于记忆。&#10;可修改！&#10;Remarks of Code,remember conveniently when multi-task switch.&#10;Modifyable!" 
-          placeholder="别名可修改">
+          placeholder="别名可改">
           <button type="button" @click="newProcess" class="btn input-group btn-success btn-outline-warning flex-shrink-0" style = "width:60px;text-align:center; padding-left:0px;padding-right:0px;"
           title="点击开始新任务,之前的任务会后台继续运行。&#10;点击Code输入框可切换任务。&#10;Start a new transfer,the transfers before will still running.&#10;Click input box of Code can switch the tasks.">New</button>
         </div>
